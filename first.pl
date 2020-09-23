34663fe527305bb439e44ede46e1b37077b1635c
TheTenzou <my@mail.com>
2020-09-23 12:03:31 +0300

    Add predicat check

--- first.pl
+++ first.pl
@@ -4,3 +4,5 @@ isPentNumber(PNum,N):-pentNumber(N,PNum).
 isPentNumber(PNum,N):-pentNumber(N,X), X< PNum, N1 is N+1, isPentNumber(PNum,N1).
 isPentNumber(PNum):-isPentNumber(PNum,1).
 
+
+check(P1,P2):-_ is P2-P1,S is P1+P2,isPentNumber(S),isPentNumber(S),write(P1),write(' '),writeln(P2).
