3dd279f2d59c857337d8f3fc2f9de271b4271412
TheTenzou <my@mail.com>
2020-09-23 11:44:38 +0300

    Add isPentNumber predicat

--- first.pl
+++ first.pl
@@ -1,3 +1,6 @@
 pentNumber(N,P):-P is N*(3*N-1)/2.
 
+isPentNumber(PNum,N):-pentNumber(N,PNum).
+isPentNumber(PNum,N):-pentNumber(N,X), X< PNum, N1 is N+1, isPentNumber(PNum,N1).
+isPentNumber(PNum):-isPentNumber(PNum,1).
 
