23a1ec41aa03add553c784b47f96fb77fa2fd280
TheTenzou <my@mail.com>
2020-09-30 10:09:56 +0300

    sm2 Add write and read list

--- second.pl
+++ second.pl
@@ -3,3 +3,8 @@ insert(Elem, M, [Elem|M]).
 
 sort1([H|T], Sorted):-sort1(T, SortedT), insert(H, SortedT, Sorted).
 sort1([],[]).
+
+writelist([]):-!.
+writelist([H|T]):-writeln(H),writelist(T).
+readlist([],0):-!.
+readlist([H|LIST],N):-read(H),N1 is N-1,readlist(LIST,N1).
