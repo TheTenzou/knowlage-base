7b79481fa7ed49f03245ac27ce8cffbeadd8bc0c
TheTenzou <my@mail.com>
2020-09-30 10:22:06 +0300

    sm2 Add run predicat

--- second.pl
+++ second.pl
@@ -5,6 +5,15 @@ sort1([H|T], Sorted):-sort1(T, SortedT), insert(H, SortedT, Sorted).
 sort1([],[]).
 
 writelist([]):-!.
-writelist([H|T]):-writeln(H),writelist(T).
+writelist([H|T]):-write(H), write(' '),writelist(T).
 readlist([],0):-!.
 readlist([H|LIST],N):-read(H),N1 is N-1,readlist(LIST,N1).
+
+run:-
+    write('size list: '),
+    read(Size),
+    write('lisr: '),
+    readlist(List, Size),
+    write('sorted list:'),
+    sort1(List,SortedList),
+    writelist(SortedList).
