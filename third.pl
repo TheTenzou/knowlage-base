6794bcd2859ea2e262ac3bdfab7c241a60f7e440
TheTenzou <my@mail.com>
2020-10-07 13:50:56 +0300

    sm3 Add read and write files

---
+++ third.pl
@@ -0,0 +1,41 @@
+% a=97,....
+% z=122
+% A=65,....
+% Z=90
+read_str(A,N,Flag):-
+    get0(X),
+    r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-
+    K1 is K+1,
+    append(B,[X],B1),
+    get0(X1),
+    r_str(X1,A,B1,N,K1,Flag).
+
+read_list_str(List):-
+    read_str(A,_,Flag),
+    read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+    read_str(A,_,Flag),
+    append(Cur_list,[A],C_l),
+    read_list_str(C_l,List,Flag).
+
+write_str([]):-!.
+write_str([H|Tail]):-
+    put(H),
+    write_str(Tail).
+
+write_list_str([]):-!.
+write_list_str([H|T]):-
+    write_str(H),nl,
+    write_list_str(T).
+
+run:-
+    see('C:/Users/Tenzou/Documents/Prolog/sm/third test read.txt'),
+    read_list_str(List),
+    seen,
+    tell('C:/Users/Tenzou/Documents/Prolog/sm/third test write.txt'),
+    write_list_str(List).
+    told.
