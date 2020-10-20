b09769f9109daf58ca6ec0de80456d712383633c
TheTenzou <my@mail.com>
2020-10-20 16:27:57 +0300

    sm5 final

--- fifth.pl
+++ fifth.pl
@@ -1,7 +1,31 @@
 combinations1([],[]).
 combinations1([H|T], [H|T2]):-combinations1(T,T2).
 combinations1([_|T], T2):-combinations1(T,T2).
-combinations(List, Result):-combinations1(List, Result), Result \= [].
+combinations(List, Result):-
+    combinations1(List, Result),
+    Result = [_,_,_].
 
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
+
+run:-
+    combinations([a,b,c,d,e,f],List),
+    tell('C:/Users/Tenzou/Documents/Prolog/sm/fifth answer.txt'),
+    generate_words(List,8,[]),
+    told.
+
+generate_words(_,0,Word):-
+    write_str(Word),nl,!,fail.
+
+generate_words(A,N,Perm):-
+    in_list(A,El),
+    N1 is N-1,
+    generate_words(A,N1,[El|Perm]).
+
+
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+
+
+
