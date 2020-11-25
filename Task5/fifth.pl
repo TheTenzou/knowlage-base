00e019d43025dc4776cd3fc83d990f5230a98618
TheTenzou <my@mail.com>
2020-11-25 19:12:34 +0300

    Fxi

--- Task5/fifth.pl
+++ Task5/fifth.pl
@@ -9,7 +9,7 @@ in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
 run:-
-    tell('C:/Users/Tenzou/Documents/Prolog/sm/Task5/fifth answer.txt'),
+    tell('C:/Users/Tenzou/Documents/Prolog/sm/Task5/fifth answer9.txt'),
     generate_all_words,
     told.
 
@@ -19,16 +19,31 @@ generate_all_words:-
 
 
 generate_words(_,0,Word):-
-    write_str(Word),nl,!,fail.
+    count_different_elements(Word,X),
+    list_length(X,L),
+    (   L = 3 ->
+    write_str(Word),nl,!,fail; !,fail).
 
 generate_words(A,N,Perm):-
     in_list(A,El),
     N1 is N-1,
     generate_words(A,N1,[El|Perm]).
 
+count_different_elements([H|T], Tmp):-
+    in_list(Tmp, H), count_different_elements(T, Tmp).
+count_different_elements([], _).
+
+list_length([]     , 0 ).
+list_length([_|Xs] , L ) :- list_length(Xs,N) , L is N+1.
+
+getSimbol(Simbol,[Simbol|Rest],Rest).
+getSimbol(Simbol,[_,Rest],R):-getSimbol(Simbol,Rest,R).
+
 
 write_str([]):-!.
 write_str([H|Tail]):-put(H),write_str(Tail).
+
+
 
 
 
