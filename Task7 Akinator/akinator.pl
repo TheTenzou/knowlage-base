52912b016fb85f5af4cd7d6222747681a78ff800
TheTenzou <my@mail.com>
2020-11-29 21:11:29 +0300

    Working with dinamic memory

--- Task7 Akinator/akinator.pl
+++ Task7 Akinator/akinator.pl
@@ -1,127 +1,77 @@
-:- dynamic high/2.
+:- dynamic q1/2.
+:- dynamic q2/2.
 
 read_str(A):-get0(X),r_str(X,A,[]).
 r_str(10,A,A):-!.
 r_str(X,A,B):-append(B,[X],B1),get0(X1),r_str(X1,A,B1).
 
-% repeat protege
 
-high_r(X,Y):-	repeat, (high(X,Y) -> (nl,write(X),nl,write(Y),write("."),
-				retract(high(X,Y))) ;X=nil,Y=nil).
+write_q1(X,Y):-
+    repeat,
+    (q1(X,Y) -> (
+         nl,
+         write(X),
+         nl,write(Y),
+         write("."),
+         retract(q1(X,Y)));
+    X=nil,
+    Y=nil).
 
-pr2:-tell('c:/Prolog/26_1_prolog_F/111.txt'),high_r(X,_),X=nil,told.
+read_q1(-1):-!.
+read_q1(_):-
+    read_str(Lang),
+    name(X,Lang),
+    read(Y),
+    asserta(q1(X,Y)),
+    get0(Sym),
+    read_q1(Sym).
 
-pr3:-see('C:/Users/Tenzou/Documents/Prolog/facts_high.txt'),get0(Sym),read_high(Sym),seen.
-read_high(-1):-!.
-read_high(_):-	read_str(Lang),name(X,Lang),read(Y),asserta(high(X,Y)),
-				get0(Sym),read_high(Sym).
 
-%high(ruby,1).
-%high(c_sharp,1).
-%high(python,1).
-%high(c_plu_plus,1).
-%high(f_sharp,1).
-%high(prolog,1).
-%high(c,1).
-%high(asm,0).
+save_q1:-
+    tell('C:/Users/Tenzou/Documents/Prolog/sm/Task7 Akinator/predicats/q1.txt'),
+    write_q1(X,_),
+    X=nil,
+    told.
 
-decl(ruby,0).
-decl(c_sharp,0).
-decl(python,0).
-decl(c_plu_plus,0).
-decl(f_sharp,1).
-decl(prolog,1).
-decl(c,0).
-decl(asm,0).
+load_q1:-
+    see('C:/Users/Tenzou/Documents/Prolog/sm/Task7 Akinator/predicats/q1.txt'),
+    get0(Sym),
+    read_q1(Sym),
+    seen.
 
-interpret(ruby,1).
-interpret(python,1).
-interpret(f_sharp,1).
-interpret(prolog,1).
-interpret(c_sharp,0).
-interpret(c_plu_plus,0).
-interpret(c,0).
-interpret(asm,0).
+question1(X1):-
+    write("Question 1?"),nl,
+    write("1. Yes"),nl,
+    write("0. NO"),nl,
+    read(X1).
 
-oop(ruby,3).
-oop(c_sharp,3).
-oop(python,2).
-oop(c_plu_plus,2).
-oop(f_sharp,1).
-oop(prolog,1).
-oop(c,0).
-oop(asm,0).
+question2(X2):-
+    write("Question 2?"),nl,
+    write("1. Yes"),nl,
+    write("0. NO"),nl,
+    read(X2).
 
-cross(ruby,1).
-cross(python,1).
-cross(c_plu_plus,1).
-cross(prolog,1).
-cross(c,1).
-cross(asm,1).
-cross(c_sharp,0).
-cross(f_sharp,0).
 
-visual(c_sharp,3).
-visual(ruby,2).
-visual(python,2).
-visual(c_plu_plus,2).
-visual(f_sharp,2).
-visual(prolog,1).
-visual(c,0).
-visual(asm,0).
-
-mobile(c_sharp,0).
-mobile(ruby,0).
-mobile(python,0).
-mobile(c_plu_plus,0).
-mobile(f_sharp,0).
-mobile(prolog,0).
-mobile(c,0).
-mobile(asm,0).
-
-question1(X1):-	write("Is your language high level?"),nl,
-				write("1. Yes"),nl,
-				write("0. NO"),nl,
-				read(X1).
-
-question2(X2):-	write("Is your language declarative?"),nl,
-				write("1. Yes"),nl,
-				write("0. NO"),nl,
-				read(X2).
-
-question3(X3):-	write("Is your language interpret?"),nl,
-				write("1. Yes"),nl,
-				write("0. NO"),nl,
-				read(X3).
-
-question4(X4):-	write("Does your language support OOP?"),nl,
-				write("3. It is OOP itself"),nl,
-				write("2. yes"),nl,
-				write("1. yes, but very hard"),nl,
-				write("0. NO"),nl,
-				read(X4).
-
-question5(X5):-	write("Is your language crossplatformic?"),nl,
-				write("1. Yes"),nl,
-				write("0. NO"),nl,
-				read(X5).
-
-question6(X6):-	write("Does your language support visual interface for user?"),nl,
-				write("3. It is visual itself"),nl,
-				write("2. yes"),nl,
-				write("1. yes, but very hard"),nl,
-				write("0. NO"),nl,
-				read(X6).
-
-question7(X7):-	write("Is your language for mobile phones?"),nl,
-				write("1. Yes"),nl,
-				write("0. NO"),nl,
-				read(X7).
+run:-
+    load_q1,
+    question1(X1),
+    %question2(X2),
+    q1(X,X1),
+    %q2(X,X2),
+    write(X).
 
 
 
-pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
-		question5(X5),question6(X6),question7(X7),
-		high(X,X1),decl(X,X2),interpret(X,X3),oop(X,X4),
-		cross(X,X5),visual(X,X6),mobile(X,X7),
-		write(X).
+
+
+
+
+
+
+
+
+
+
+
+
+
