ad829194869f1d2d1b214ab799d1485453280493
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-12-08 15:40:51 +0300

    Add properties and move akinator

---
+++ Task8-Akinator-with-interface/src/test/java/ru/thetenzou/akinator/AkinatorTest.java
@@ -0,0 +1,11 @@
+package ru.thetenzou.akinator;
+
+import org.junit.jupiter.api.Test;
+
+public class AkinatorTest {
+
+    @Test
+    public void questionTest() {
+
+    }
+}
--- Task8-Akinator-with-interface/src/test/java/ru/thetenzou/jplTest.java
+++ Task8-Akinator-with-interface/src/test/java/ru/thetenzou/jplTest.java
@@ -10,17 +10,18 @@ public class jplTest {
     @Test
     public void testJpl() {
         JPL.init();
-        String s = "consult('E:/My_Progs/knowlage-base/Task8-Akinator-with-interface/src/main/prolog/akinator.pl')";
+        String s = "consult('E:/My_Progs/knowlage-base/Task8-Akinator-with-interface/src/main/resources/akinator.pl')";
         Query q1 = new Query(s);
         System.out.println(q1.hasSolution());
         Query q2 = new Query("load");
         System.out.println(q2.hasSolution());
-//        Variable X = new Variable("X");
-//        Query q3 = new Query("q1", new Term[] {X,new Integer(1)});
+        Variable X = new Variable("X");
+        Query q3 = new Query("q1", new Term[] {X,new Integer(5)});
 //        Map<String, Term>[] solutions = q3.allSolutions();
 //        for (Map<String, Term> solution : solutions) {
 //            System.out.println(solution.get("X"));
 //        }
+        System.out.println("no solution " + q3.hasSolution());
 //        System.out.println(q3.oneSolution().get("X"));
         Query q4 = new Query("assert", new Term[] {
                 new Atom("test"),
@@ -36,10 +37,10 @@ public class jplTest {
                 new Integer(1)
         });
         System.out.println(q4.hasSolution());
-        Variable X = new Variable("X");
-        Query q5 = new Query("q1", new Term[] {new Atom("test"), X});
+        Variable X1 = new Variable("X");
+        Query q5 = new Query("q1", new Term[] {new Atom("test"), X1});
         System.out.println(q5.oneSolution().get("X"));
-        Query q6 = new Query("save");
-        System.out.println(q6.hasSolution());
+//        Query q6 = new Query("save");
+//        System.out.println(q6.hasSolution());
     }
 }
